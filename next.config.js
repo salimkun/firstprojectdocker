0acedeb73e030117633d53e8b965c68f7d9598aa
Salimkun <m.alimarrasyid@gmail.com>
2019-05-20 13:56:24 +0700

    just update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     target: 'serverless'
   }
+
+// for export to static
+// module.exports = {
+//   exportPathMap: function() {
+//     return {
+//       '/': { page: '/' }
+//     }
+//   }
+// }
